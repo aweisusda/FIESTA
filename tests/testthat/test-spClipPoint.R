959368a90a7d0c65dfff9ba18efc2a60d84015cf
aweisusda <147875668+aweisusda@users.noreply.github.com>
2023-10-23 11:23:31 -0500

    Add spClipPoint Test

---
+++ tests/testthat/test-spClipPoint.R
@@ -0,0 +1,21 @@
+
+test_that("Clip Point returns expected number of points", {
+
+  WYbhfn <- system.file("extdata",
+                      "sp_data/WYbighorn_adminbnd.shp",
+                      package = "FIESTA")
+
+  WYbh <- spImportSpatial(WYbhfn)
+
+  clipped_feature <- spClipPoint(xyplt = WYplt,
+                                 uniqueid = "CN",
+                                 clippolyv = WYbh,
+                                 spMakeSpatial_opts = list(xvar = "LON_PUBLIC",
+                                                           yvar = "LAT_PUBLIC",
+                                                           xy.crs = 4269))
+
+  expected_num <- 56
+  clipped_num <- length(clipped_feature$clip_xyplt$CN) # Num of Features
+
+  expect_equal(clipped_num, expected_num)
+})
