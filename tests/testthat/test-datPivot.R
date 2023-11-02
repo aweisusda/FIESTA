c7c46f28c12a443e54aa22754ac386586dcb60ef
aweisusda <147875668+aweisusda@users.noreply.github.com>
2023-11-02 09:11:33 -0500

    Adjust datPivot Test

--- tests/testthat/test-datPivot.R
+++ tests/testthat/test-datPivot.R
@@ -24,14 +24,12 @@ test_that("datPivot Testing", {
                    xfilter = "STATUSCD == 1")
   
     CN <- 40404730010690
-    test2_subset <- test2[test2$PLT_CN == CN, "113",][[1]]
-    test2_subset_char <- as.character(test2_subset)
+    test2_subset <- round(test2[test2$PLT_CN == CN, "113",][[1]], 2)
     
     input1 <- WYtree[WYtree$PLT_CN == CN, ]
-    input2 <- sum(input1[input1$SPCD == 113 & input1$STATUSCD == 1, "VOLCFNET"])
-    input3_final <- formatC(input2)
+    input2 <- round(sum(input1[input1$SPCD == 113 & input1$STATUSCD == 1, "VOLCFNET"]), 2)
     
-    expect_equal(test2_subset_char, input3_final)
+    expect_equal(test2_subset, input2)
     expect_equal(dim(test1), c(10,4))
     expect_snapshot(test1)
 })
